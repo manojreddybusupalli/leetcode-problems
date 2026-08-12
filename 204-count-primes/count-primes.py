7a389a17d2f6979a5407e22baf0abe7358ff7e66
Busupalli Manoj Reddy <manojreddybusupalli@gmail.com>
2026-08-12 10:01:10 +0530

    Time: 2599 ms (32.89%) | Memory: 130.4 MB (80.44%) - LeetSync

---
+++ 204-count-primes/count-primes.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countPrimes(self, n):
+        if n <= 2:
+            return 0
+        is_prime = [True] * n
+        is_prime[0] = is_prime[1] = False
+
+        p = 2
+        while  p * p < n:
+            if is_prime[p]:
+                for i in range(p * p, n,p):
+                    is_prime[i]=False
+            p += 1
+
+        return sum(is_prime)
+
+
+    
